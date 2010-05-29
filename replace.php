ea7d39be13bc65ca949be2b3e228b3211cf020f0
Johan Sageryd <j416@1616.se>
2010-05-29 15:13:58 +0200

    Build build build

--- replace.php
+++ replace.php
@@ -1,3 +1,4 @@
+<?
 /*
 	Replace plugin for MODx
 	-----------------------
@@ -12,10 +13,69 @@
 
 */
 
+/* Start testing */
+$s = <<<EOD
+|replace|
+	tomato => apple
+	hamburger => sushi
+|/replace|
+This is between two replace sections.
+|replace|
+	dog => monkey
+	horse => cat
+	sushi => monkey
+|/replace|
+|replace|
+	apa => bepa
+|/replace|
+The cat likes hamburgers and tomato sauce and it also like sushis.
+EOD;
+
+print replace($s);
+/* End testing */
+
 function replace($string){
-	return $string;
+	$replacepreamble = stripreplacepreamble($string);
+
+	// Replace
+	$s = $string;
+	$padding = '§';
+	foreach($replacepreamble as $k => $v){
+		$s = preg_replace('/(?<!' . $padding .')' . preg_quote($k) . '(?!' . $padding . ')/uU', $padding . preg_quote($v) . $padding, $s);
+	}
+
+	// Remove all padding
+	$s = preg_replace('/'.$padding.'/u', '', $s);
+
+	return $s;
 }
 
+// Returns an hash with all replacements to be made
+function stripreplacepreamble(&$string){
+	// Get all |replace| |/replace| sections
+	preg_match_all("/\|replace\|\s*?(.*)\s*?\|\/replace\|/uisU", $string, $sections);
+
+	// For each section, add each get each of its key-value pairs and add to $replacepreamble
+	$replacepreamble = array();
+	foreach($sections[1] as $section){
+		preg_match_all("/\s*(.*=>.*)\s*/u", $section, $entrysets);
+		foreach($entrysets[1] as $entries){
+			$entry = (preg_split("/\s*=>\s*/u", $entries));
+			$replacepreamble[$entry[0]] = $entry[1];
+		}
+	}
+
+	// Remove all |replace| |/replace| sections from the original string
+	do{	
+		$laststring = $string;
+		$string = preg_replace("/(\|replace\|.*\|\/replace\|\s)/uisU", '', $string);
+	}while($string !== $laststring);
+
+	// Return hash
+	return $replacepreamble;
+}
+
+/*
 $e = &$modx->Event;
 switch ($e->name) {
 	case "OnLoadWebDocument":
@@ -26,3 +86,5 @@ switch ($e->name) {
 		return;
 		break;
 }
+*/
+?>

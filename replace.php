d1326bde4bf03c284304ae2c1bb6984ae839ef57
Johan Sageryd <j416@1616.se>
2010-06-03 18:08:01 +0200

    Generalise names

--- replace.php
+++ replace.php
@@ -14,23 +14,23 @@
 
 function replace($string){
 	// Get hash of things to replace
-	$replacepreamble = stripreplacepreamble($string);
+	$sectionscontent = sectionstohash($string, 'replace');
 
 	// Sort its keys by length, longest word first to prevent substring replacement
-	uksort($replacepreamble, function ($a, $b){ return mb_strlen($a, 'UTF-8') < mb_strlen($b, 'UTF-8'); });
+	uksort($sectionscontent, function ($a, $b){ return mb_strlen($a, 'UTF-8') < mb_strlen($b, 'UTF-8'); });
 
 	// Replace
 	$s = $string;
 	$counter = 0;
 	// First replace with a padded number to prevent clashes
 	$padding = '§§§§§§§§§§';
-	foreach($replacepreamble as $k => $v){
+	foreach($sectionscontent as $k => $v){
 		$s = preg_replace('/' . preg_quote($k) . '/uU', $padding . $counter . $padding, $s);
 		$counter++;
 	}
 	// Then replace that padded number with the actual value
 	$counter = 0;
-	foreach($replacepreamble as $k => $v){
+	foreach($sectionscontent as $k => $v){
 		$s = preg_replace('/' . $padding . $counter . $padding . '/uU', $v, $s);
 		$counter++;
 	}
@@ -38,29 +38,29 @@ function replace($string){
 	return $s;
 }
 
-// Returns an hash with all replacements to be made
-function stripreplacepreamble(&$string){
-	// Get all |replace| |/replace| sections
-	preg_match_all("/\|replace\|\s*?(.*)\s*?\|\/replace\|/uisU", $string, $sections);
+// Returns a hash with all section content and removes the section(s) from the string
+function sectionstohash(&$string, $sectionidentifier){
+	// Get all |sectionidentifier| |/sectionidentifier| sections
+	preg_match_all("/\|$sectionidentifier\|\s*?(.*)\s*?\|\/$sectionidentifier\|/uisU", $string, $sections);
 
-	// For each section, add each get each of its key-value pairs and add to $replacepreamble
-	$replacepreamble = array();
+	// For each section, add each get each of its key-value pairs and add to $returnhash
+	$returnhash = array();
 	foreach($sections[1] as $section){
 		preg_match_all("/\s*(.*=>.*)\s*/u", $section, $entries);
 		foreach($entries[1] as $entry){
 			$keyvaluepair = (preg_split("/\s*=>\s*/u", $entry));
-			$replacepreamble[chop($keyvaluepair[0])] = chop($keyvaluepair[1]);
+			$returnhash[chop($keyvaluepair[0])] = chop($keyvaluepair[1]);
 		}
 	}
 
-	// Remove all |replace| |/replace| sections from the original string
+	// Remove all |sectionidentifier| |/sectionidentifier| sections from the original string
 	do{	
 		$laststring = $string;
-		$string = preg_replace("/(\|replace\|.*\|\/replace\|\s)/uisU", '', $string);
+		$string = preg_replace("/(\|$sectionidentifier\|.*\|\/$sectionidentifier\|\s)/uisU", '', $string);
 	}while($string !== $laststring);
 
 	// Return hash
-	return $replacepreamble;
+	return $returnhash;
 }
 
 $e = &$modx->Event;
